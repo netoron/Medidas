c80867af2c22583f805c16884614cf05af594ce2
netoron <61334943+netoron@users.noreply.github.com>
2020-05-24 02:28:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string medida;
+            double km = 0;
+            double soma2;
+            
+            Console.WriteLine("Digite uma medida em metros para ser convertida em km e cm");
+            medida = Console.ReadLine();
+            soma2 = Convert.ToDouble(medida); 
+            Console.WriteLine($"O Total em km é {km},{medida}Km");
+            Console.WriteLine($"O Total em cm é {medida}00cm");
+
+
+            
+        }
+    }
+}
